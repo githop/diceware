2a54e30beea124034b953e96cb2db65e1528b96d
Tom Hopkins <tom@githop.com>
2015-10-26 13:20:07 -0600

    refactor - Die constructor fn into revealing module change - more sensible method names for Die module.

--- diceware.js
+++ diceware.js
@@ -1,24 +1,25 @@
 var dict = require('./wordsHandler');
-var Die = require('./die');
+var Die = require('./die')();
 var arg = process.argv[2];
-var DICE = new Die();
 
-(arg == undefined) ? arg = 5 : arg;
-
+//uses Die and dict modules to simulate
+//the process of using diceware to generate passwords
+//accepts num param to set length of generated pw.
 function generatePassPhrase(len) {
   var pw = [];
-  //the dict is a JS object where the key is a five digit int
-  //and the val is the word
-  //To get a word from the dict, roll a 6 sided dice 5 times
-  //to generate a key to fetch the word with.
+
+  if (len == undefined) len = 5;
   var input = parseInt(len, 0);
+
   if (isNaN(input)) {
-    console.log('First Param must be a Number!\n\nUsing default (5) password length.\n');
+    console.log('First Param must be a Number!\n\nUsing default (5) word password length.\n');
     input = 5;
   }
-  DICE.times(input).forEach(function(n) {
+
+  Die.generateDicewareNums(input).forEach(function(n) {
     pw.push(dict[n]);
   });
+
   return pw.join(' ');
 }
 
--- die.js
+++ die.js
@@ -3,38 +3,52 @@
  * Created by githop on 10/25/15.
  */
 
-function Die(sides) {
-  this.sides = sides || 6;
-}
+//who needs prototypes anyway?
+var Die = function(sides) {
+  var sides = sides || 6;
 
-//returns random number between 1 - 6 (or input die size)
-Die.prototype.roll = function() {
-  return 1 + Math.floor(Math.random() * this.sides);
-};
+  //'rolls' a die one time
+  //returns int range 1-6 (or input side length)
 
-//returns string, will 'roll die' n times and
-//return results
-Die.prototype.rolls = function(n) {
-  var rls = [];
-  for (var i = 0; i < n; i++) {
-    rls.push(this.roll());
+  function roll() {
+    return 1 + Math.floor(Math.random() * sides);
   }
-  return rls.join('');
-};
 
-//returns 2d array, params (times, rollsCount)
-//returns array of length n, of die rolled p times
-Die.prototype.times = function(n, p) {
-  if (p === undefined) {
-    p = 5;
+  //'rolls' a single die n times,
+  //returns results of each roll as single string
+  //e.g. rolls(3) => '243';
+
+  function rolls(n) {
+    var rls = [];
+    for (var i = 0; i < n; i++) {
+      rls.push(roll());
+    }
+    return rls.join('');
   }
-  var times = [];
-  for (var i = 0; i < n; i++) {
-    times.push(this.rolls(p))
+
+  //returns an array of 'rolls'
+  //first param - phrase length
+  //second param - rolls count
+  //used to naively generate id strings to match words from the diceware dictionary
+  //e.g. generateDicewareNums(2) => ['34253', '22431']
+
+  function generateDicewareNums(n, p) {
+    if (p === undefined) {
+      p = 5;
+    }
+    var t = [];
+    for (var i = 0; i < n; i++) {
+      t.push(rolls(p));
+    }
+    return t;
   }
-  return times;
+
+  return {
+    roll: roll,
+    rolls: rolls,
+    generateDicewareNums: generateDicewareNums
+  };
+
 };
 
 module.exports = Die;
-
-
